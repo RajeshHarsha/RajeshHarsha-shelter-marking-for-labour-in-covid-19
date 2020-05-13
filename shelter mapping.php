fc079900e507d9a494c627de951060819a2ef92d
Rajesh Routhu <44241472+RajeshHarsha@users.noreply.github.com>
2020-05-13 19:25:34 +0530

    Create shelter mapping.php

---
+++ shelter mapping.php
@@ -0,0 +1,42 @@
+<?php
+$location = $_POST['Location'];
+
+$email = $_POST['email'];
+
+$phone = $_POST['phone'];
+if (!empty($location)  || !empty($email)  || !empty($phone)) {
+ $host = "localhost";
+    $dbUsername = "root";
+    $dbPassword = "";
+    $dbname = "labor_shelter_mapping";
+    //create connection
+    $conn = new mysqli($host, $dbUsername, $dbPassword, $dbname);
+    if (mysqli_connect_error()) {
+     die('Connect Error('. mysqli_connect_errno().')'. mysqli_connect_error());
+    } else {
+     $SELECT = "SELECT email From register Where email = ? Limit 1";
+     $INSERT = "INSERT Into register (location, email, phone) values(?, ?, ?)";
+     //Prepare statement
+     $stmt = $conn->prepare($SELECT);
+     $stmt->bind_param("s", $email);
+     $stmt->execute();
+     $stmt->bind_result($email);
+     $stmt->store_result();
+     $rnum = $stmt->num_rows;
+     if ($rnum==0) {
+      $stmt->close();
+      $stmt = $conn->prepare($INSERT);
+      $stmt->bind_param("ssssii", $location,$email, $phone);
+      $stmt->execute();
+      echo "New record inserted sucessfully";
+     } else {
+      echo "Someone already register using this email";
+     }
+     $stmt->close();
+     $conn->close();
+    }
+} else {
+ echo "All field are required";
+ die();
+}
+?>
